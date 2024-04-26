669afe9c7130c37abb66e513a3fa43926a36f1d5
Sandeepa123 <sandeeparathnayaka20@gmail.com>
2024-04-26 22:41:51 +0530

    3API'S done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import MarsRover from "./components/MarsRover";
+import APOD from "./components/APOD";
+import CME from "./components/CME";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -12,6 +15,9 @@ function App() {
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
+			<Route path="/apod" exact element={<APOD />} />
+			<Route path="/cme" exact element={<CME />} />
+			<Route path="/marseRover" exact element={<MarsRover />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
 	);
